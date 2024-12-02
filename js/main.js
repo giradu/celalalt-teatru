37e08bdcdf4d994ee6d6b056a3ef44d85a1fd65c
radu_g <radu.giurgiu@saltandpepper.co>
2024-12-02 22:39:15 +0200

    landing page deschidere

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate = '2020/04/07';
+        var finalDate = '2024/12/06';
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
